b536bf44c047190cb4ab40cd6d52119414a46c29
BuildTools <unconfigured@null.spigotmc.org>
2021-06-21 11:44:42 +0800

    awful code :(

--- src/main/java/me/discord/api/Discord.java
+++ src/main/java/me/discord/api/Discord.java
@@ -3,36 +3,26 @@ package me.discord.api;
 import lombok.Getter;
 import lombok.Setter;
 import me.discord.api.handlers.commandhandler.Command;
-import me.discord.api.handlers.commandhandler.CommandListener;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.security.auth.login.LoginException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public abstract class Discord {
     @Getter @Setter
-    private static String token = "ODU1NzIwOTA1NTQwMjM5MzYw.YM2mMg.4kd1Ej5_OCauE6W8XcR6NyiNDM8";
-    @Getter
+    private static String token = "";
+    @Getter @Setter
     private static JDA jda = null;
     @Getter
     private static final List<Command> commands = new ArrayList<>();
 
-    static {
-        try {
-            jda = JDABuilder.createDefault(token).build();
-            registerEvents(new CommandListener());
-        } catch (LoginException e) {
-            e.printStackTrace();
-        }
-    }
-
     protected static void registerEvents(ListenerAdapter... listenerAdapter) {
         Arrays.stream(listenerAdapter).forEach(jda::addEventListener);
     }
+
+
 
     public static Command get(String name) {
         return commands
